56b6f1eeaf928c7ffd1c45e919738689d532fa79
anujkmehrotra <anujkmehrotra@gmail.com>
2022-09-01 13:32:21 +0530

    updated name

--- .bakpkgs.sh
+++ .bakpkgs.sh
@@ -9,7 +9,7 @@ rm -f $bakdir/optdeplist.txt && comm -13 <(pacman -Qqdt | sort) <(pacman -Qqdtt 
 echo "All optional packages listed in the file 'optdeplist'."
 rm -f $bakdir/aurpkglist.txt && pacman -Qmq > $bakdir/aurpkglist.txt
 echo "All aur built packages listed in the file 'aurpkglist'."
-#   Removing my manual build pkg (CK kernel) name from the localpkglist
+#   Removing my manual build pkg (xanmod) name from the localpkglist
 sed -i '/linux-xanmod/d' $bakdir/aurpkglist.txt
 #sed -i '/linux-xanmod/d' $bakdir/pkglist.txt
 
